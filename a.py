ec32f194df2ac20b621a1667785b5f0638dac32c
mehmet kılınc <75855428+MehmetKilinc@users.noreply.github.com>
2022-02-20 02:06:59 +0300

    Add files via upload

---
+++ a.py
@@ -0,0 +1,53 @@
+import hashlib
+
+path = r"/root/shel.php"
+
+with open(path , 'rb') as file:          #b is binary and r is read
+
+	content = file.read()
+	md5 = hashlib.md5()
+	sha1 = hashlib.sha1()
+	sha224 = hashlib.sha224()
+	sha256 = hashlib.sha256()
+	sha384 = hashlib.sha384()
+	sha512 = hashlib.sha512()
+
+
+	md5.update(content)
+	sha1.update(content)
+	sha224.update(content)
+	sha256.update(content)
+	sha384.update(content)
+	sha512.update(content)
+
+	print("{}  :  {}".format(md5.name , md5.hexdigest()))
+	print("{}  :  {}".format(sha1.name , sha1.hexdigest()))
+	print("{}  :  {}".format(sha224.name , sha224.hexdigest()))
+	print("{}  :  {}".format(sha256.name , sha256.hexdigest()))
+	print("{}  :  {}".format(sha384.name , sha384.hexdigest()))
+	print("{}  :  {}".format(sha512.name , sha512.hexdigest()))
+
+
+#***************************************************************************************
+
+with open(path , 'rb') as file:
+
+	content = file.read()
+
+	list1 = [md5 , sha1 , sha224 , sha256 , sha384 , sha512]
+
+	for i in list1:
+
+		i.update(content)
+		print("{}  :  {}".format(i.name , i.hexdigest()))
+
+#*****************************************************************************************
+
+for i in liste1:
+
+	with open(path , 'rb') as file:
+
+		for j in file:
+
+			i.update(j)
+		print("{}  :  {}".format(i.name , i.hexdigest()))
